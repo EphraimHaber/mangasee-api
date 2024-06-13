053c1edd69d59e623a0114793d7b17631d0283eb
Itamar Shubin <itamarshubin@gmail.com>
2024-06-13 18:43:29 +0300

    use xml parser

--- src/utils.ts
+++ src/utils.ts
@@ -1,5 +1,7 @@
 import axios from 'axios';
+import { parseString } from 'xml2js';
 import { mangaCoverBaseUrl, mangaReadOnlineBaseUrl, rssBaseUrl } from './constants';
+import { XMLParser } from 'fast-xml-parser';
 
 export const getCoverImageUrl = (uri: string): string => {
   return `${mangaCoverBaseUrl}${uri}.jpg`;
@@ -13,5 +15,8 @@ export const getTitleAndCoverUrl = async (canonicalName: string) => {
   const link = `${rssBaseUrl}${canonicalName}.xml`;
   console.log(link);
   const res = (await axios.get(link)).data;
-  console.log(res);
+  const parser = new XMLParser();
+  let jObj = parser.parse(res);
+  console.log(jObj);
+  console.log(jObj.rss.channel.image);
 };
