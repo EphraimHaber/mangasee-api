e9f81366536b65d2322f79beeb19b3e6415bc84b
Itamar Shubin <itamarshubin@gmail.com>
2024-06-13 17:27:21 +0300

    prettier

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,26 @@
-import axios from "axios";
-import { mangaSeeSearch } from "./constants";
-import { getCoverImageUrl, getFirstChapterUrl } from "./utils";
-import { MangaRecord, RawMangaRecord } from "./types";
-import Fuse from "fuse.js";
+import axios from 'axios';
+import { mangaSeeSearch } from './constants';
+import { getCoverImageUrl, getFirstChapterUrl } from './utils';
+import { MangaRecord, RawMangaRecord } from './types/types';
+import Fuse from 'fuse.js';
 
 const mangaRecords: MangaRecord[] = [];
 
 export const getMangaList = async () => {
   const res: RawMangaRecord[] = (await axios.get(mangaSeeSearch)).data;
-  res.forEach((rawMangaRecord) => {
+  res.forEach(rawMangaRecord => {
     mangaRecords.push({
       canonicalName: rawMangaRecord.i,
       fullName: rawMangaRecord.s,
       nicknames: [...rawMangaRecord.a],
-      coverUrl: getCoverImageUrl(rawMangaRecord.i)
+      coverUrl: getCoverImageUrl(rawMangaRecord.i),
     });
   });
 };
 
 export const search = (term: string) => {
   const fuse = new Fuse(mangaRecords, {
-    keys: ["nicknames", "fullName"],
+    keys: ['nicknames', 'fullName'],
     threshold: 0.3,
     includeScore: true,
     isCaseSensitive: false,
@@ -30,20 +30,19 @@ export const search = (term: string) => {
   return res;
 };
 
-
-export const getMangaDetails = async (canonicalName: string = "Skeleton-Double") => {
-  const firstChapterUrl = getFirstChapterUrl(canonicalName)
+export const getMangaDetails = async (canonicalName: string = 'Skeleton-Double') => {
+  const firstChapterUrl = getFirstChapterUrl(canonicalName);
   const res = (await axios.get(firstChapterUrl)).data as string;
   const chapterDetailsPattern = /vm\.CHAPTERS = (.*);/;
-  const chaptersGroups = chapterDetailsPattern.exec(res)
-  console.log("************************")
+  const chaptersGroups = chapterDetailsPattern.exec(res);
+  console.log('************************');
   // 1: {}
   // 2: {}
   // [{"Chapter":"100010","Type":"Chapter","Page":"73","Directory":"","Date":"2022-09-12 17:54:04","ChapterName":null}]
   if (chaptersGroups) {
     console.log(chaptersGroups[1]);
   }
-}
+};
 
 (async () => {
   await getMangaList();
--- src/utils.ts
+++ src/utils.ts
@@ -1,9 +1,9 @@
-import { mangaCoverBaseUrl, mangaReadOnlineBaseUrl } from "./constants"
+import { mangaCoverBaseUrl, mangaReadOnlineBaseUrl } from './constants';
 
 export const getCoverImageUrl = (canonicalName: string): string => {
-    return `${mangaCoverBaseUrl}${canonicalName}.jpg`
-}
+  return `${mangaCoverBaseUrl}${canonicalName}.jpg`;
+};
 
 export const getFirstChapterUrl = (canonicalName: string): string => {
-    return `${mangaReadOnlineBaseUrl}${canonicalName}-chapter-1.html`
-}
+  return `${mangaReadOnlineBaseUrl}${canonicalName}-chapter-1.html`;
+};
