febc94ac2fbd85949e5f9852a1edf4f4df6d74d4
EphraimHaber <ephraimxyz@gmail.com>
2025-04-01 12:01:35 +0300

    removed unnecessary newline in constants file

--- src/constants.ts
+++ src/constants.ts
@@ -5,6 +5,5 @@ export const mangaReadOnlineBaseUrl = 'https://mangasee123.com/read-online/';
 export const mangaMetadataBaseUrl = 'https://mangasee123.com/manga/';
 export const rssBaseUrl = 'https://mangasee123.com/rss/';
 export const mangaSlideBaseUrl = 'https://scans-hot.leanbox.us/manga/';
-
 export const chapterWithPaddingLength = 4;
 export const slideWithPaddingLength = 3;
